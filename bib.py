76ab564bc054b371f85f36d71c0137b06cbff064
Haiku-na <appoline.vautrin@gmail.com>
2025-02-10 11:33:26 +0100

    premier commit

---
+++ bib.py
@@ -0,0 +1,14 @@
+# class Livre:
+#     #dunder
+#     def __init__(self):
+#         self.titre = 'title'
+#         self.auteur = 'author'
+
+
+
+def process_trucs(): 
+    pass
+
+if __name__ == '__main__': 
+    input_user = input('Que voulez-vous faire? ')
+    process_trucs()
