83e443bda488c5bd4c021a4e092f043ed1a43f27
Haiku-na <appoline.vautrin@gmail.com>
2025-02-10 15:33:16 +0100

    ca marche

--- bib.py
+++ bib.py
@@ -1,71 +1,69 @@
-import json
-# import jsonpickle
-class Book:
-    #dunder
-    def __init__(self, id, title, author, content):
-        self.__titre = title
-        self.__auteur = author
-        self.__contenu = content
-        self.__id = id #do not expose the id
-        
-    @property
-    def title(self):
-        return self.__title
-    @property
-    def author(self):
-        return self.__author
-    @property
-    def content(self):
-        return self.__content
-    @property
-    def id(self):
-        return self.__id
-        
-    @title.setter
-    def title(self, value):
-        self.title = value
+import jsonpickle
+from models import Book, BookStore, Library
 
-    @author.setter
-    def author(self, value):
-        self.author = value
-
-    @content.setter
-    def content(self, value):
-        self.content = value
-
-    @id.setter
-    def id(self, value):
-        self.id = value
-        
-
-class BookStore:
-    pass
-class Library:
-    pass
 class User:
     pass
 
+class App:
 
-def list_books():
-    print(book_store.list())
+    def __init__(self):
+        self.__actions = {
+        'ls': self.list_books,
+        'new': self.new_book,
+        'del': self.delete_book,
+        'get': self.get_book,
+        'save': self.save_to_disk,
+        'exit': self.should_continue
+        }
+        self.__book_store = BookStore()
+        self.__library = Library()
+        self.__next_id = 0
 
-def add_book():
-    title = input("Title: ")
-    author = input("Author: ")
-    content = input("Content: ")
-    book = Book(0, title, author, content)
-    book_store.add(book)
+    def should_continue(self):
+        return False
 
-def del_book():
-    id = input("Id: ")
-    book_store.delete(id)
+    def list_books(self):
+        self.__book_store.list()
+        return True
 
-if __name__ == '__main__': 
-    book_store = BookStore()
-    input_action = input("Action: ")
-    actions = {
-        "ls":list_books,
-        "new":add_book,
-        "del":del_book,
-        } 
-    print(actions[input_action]())
+    def new_book(self):
+        title = input('title: ')
+        author = input('author: ')
+        content = input('content: ')
+        book = Book(self.__next_id, author, title, content)
+        self.__book_store.add(book)
+        self.__next_id += 1
+        return True
+
+    def delete_book(self):
+        book_id = int(input('Book id: '))
+        self.__book_store.delete(book_id)
+        return True
+
+    def get_book(self):
+        book_id = input('Book id: ')
+        book = self.__book_store.get(book_id)
+        print(book)
+        return True
+
+    def save_to_disk(self):
+        # unpacking
+        for filename, obj in [('my_lib.json', self.__library), ('my_book_store.json', self.__book_store)]:
+            with open(filename, 'w') as lib_file:
+                raw_json = jsonpickle.encode(obj)
+                lib_file.write(raw_json)
+        return True
+
+    def run(self):
+        should_continue = True
+        while should_continue:
+            action = input('Action? ')
+            if action in self.__actions:
+                should_continue = self.__actions[action]()
+            else:
+                print(f'Action not supported {action}')
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
+    
